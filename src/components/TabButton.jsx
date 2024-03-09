a155f29753308f808105840468ba9c1cf172ded0
shin_21 <shinorbeta21@gmail.com>
2024-03-09 17:37:27 +0900

    first commit

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,22 @@
+export default function TabButton ({ children, onSelect, isSelected }) {
+    
+    return (
+    <li>
+        <button className={isSelected? "active" : undefined} onClick={onSelect} >{children}</button>
+    </li>
+    );
+}
+
+// export default function TabButton (props) {
+//     return (
+//     <li><button>{props.children}</button></li>
+//     );
+// }
+
+
+
+
+// JavaScript Vanilla
+//    document.querySelector("button").addEventListener("click", () => {
+    
+//    })
